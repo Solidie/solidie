a5c2d1103cc331b133e90d834fbf7811e8cec2b1
JK <jayedulk@gmail.com>
2024-07-03 17:19:51 +0600

    Popularity trend function

--- classes/Models/Popularity.php
+++ classes/Models/Popularity.php
@@ -7,6 +7,8 @@
 
 namespace Solidie\Models;
 
+use Solidie\Helpers\_Array;
+
 /**
  * Popularity class
  */
@@ -38,5 +40,47 @@ class Popularity {
 	 */
 	public static function deleteByContentId( $content_id ) {
 		Field::popularity()->deleteField( array( 'content_id' => $content_id ) );
+	}
+
+	/**
+	 * Get download trend graph especially for reports page
+	 *
+	 * @return array
+	 */
+	
+	/**
+	 * Get sales trend per month
+	 *
+	 * @return array
+	 */
+	public static function getTrend() {
+
+		global $wpdb;
+		$trends = $wpdb->get_results(
+			"SELECT 
+				DATE_FORMAT(download_date, '%Y-%m') AS download_month, 
+				COUNT(download_id) AS total_download
+			FROM 
+				{$wpdb->solidie_popularity}
+			GROUP BY 
+				download_month
+			ORDER BY 
+				download_month ASC",
+			ARRAY_A
+		);
+
+		$trends = _Array::castRecursive( $trends );
+
+		$trend_arry = array(
+			'data'   => array(),
+			'labels' => array()
+		);
+
+		foreach ( $trends as $trend ) {
+			$trend_arry['data'][]   = $trend['total_download'];
+			$trend_arry['labels'][] = $trend['download_month'];
+		}
+
+		return $trend_arry;
 	}
 }
